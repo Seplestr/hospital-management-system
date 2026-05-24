572f300d2b4ae0e5bb6f2d97bb8b70b1478b9b14
Seplestr <your_email@example.com>
2026-05-24 15:55:32 +0530

    added env

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,8 @@
+import os
+
 from pymongo import MongoClient
 
-MONGO_URL = "mongodb+srv://pandeypr3_db_prab:nJtErB0H5S5Zu2XI@cluster0.4015w2f.mongodb.net/?appName=cluster0"
+MONGO_URL = os.getenv("MONGO_URL")
 
 client = MongoClient(MONGO_URL)
 
